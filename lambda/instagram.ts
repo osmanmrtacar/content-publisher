80919e682dd7b2dd2ba705574423142fe27127f7
osmanmrtacar <osmanmrtacar@yandex.com>
2023-06-16 15:59:05 +0000

    share on instagram

---
+++ lambda/instagram.ts
@@ -0,0 +1,35 @@
+import {Handler} from 'aws-lambda';
+import * as facebook from 'facebook-nodejs-business-sdk';
+
+export const handler: Handler = async (event, context) => {
+  console.log(event);
+  const accessToken = process.env.ACCESS_TOKEN;
+
+  const api = facebook.FacebookAdsApi.init(accessToken);
+
+  const user = new facebook.IGUser('17841404434626251');
+
+  try {
+    // const media = await user.createMedia([], {
+    //   image_url:
+    //     'https://ichef.bbci.co.uk/news/800/cpsprodpb/c4f0/live/ace78480-07ab-11ee-b5af-25e80c61c11a.jpg',
+    //   caption:
+    //     'Tom Holland, en son rol aldığı dizi yüzünden oyunculuğa bir yıl ara veriyor',
+    // });
+    // const result = await user.createMediaPublish([], {
+    //   creation_id: media.id,
+    // });
+    // console.log(result.id);
+  } catch (error) {
+    console.log(error.message);
+
+    return {
+      statusCode: 400,
+      message: error.message,
+    };
+  }
+
+  return {
+    statusCode: 200,
+  };
+};
--- lib/social-media-content-cdk-stack.ts
+++ lib/social-media-content-cdk-stack.ts
@@ -1,16 +1,127 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Construct} from 'constructs';
+import * as ApiGateway from 'aws-cdk-lib/aws-apigateway';
+import {EndpointType} from 'aws-cdk-lib/aws-apigateway';
+import * as IAM from 'aws-cdk-lib/aws-iam';
+import * as Lambda from 'aws-cdk-lib/aws-lambda';
+import * as StepFunctions from 'aws-cdk-lib/aws-stepfunctions';
+import * as Tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
+
+import {NodejsFunction} from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as path from 'path';
 
 export class SocialMediaContentCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const discordApiGateway = new ApiGateway.RestApi(this, 'DiscordGateway', {
+      restApiName: 'Discord Api Gateway',
+      description: 'Welcomes discord commands',
+      endpointConfiguration: {
+        types: [EndpointType.REGIONAL],
+      },
+      deploy: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SocialMediaContentCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const lambdaRole = new IAM.Role(this, 'SocialMediaContentLambdaRole', {
+      roleName: 'social-media-content-lambda-role',
+      assumedBy: new IAM.ServicePrincipal('lambda.amazonaws.com'),
+    });
+
+    lambdaRole.addManagedPolicy(
+      IAM.ManagedPolicy.fromAwsManagedPolicyName(
+        'service-role/AWSLambdaBasicExecutionRole'
+      )
+    );
+    lambdaRole.addManagedPolicy(
+      IAM.ManagedPolicy.fromAwsManagedPolicyName(
+        'service-role/AWSLambdaVPCAccessExecutionRole'
+      )
+    );
+    lambdaRole.addManagedPolicy(
+      IAM.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMReadOnlyAccess')
+    );
+    lambdaRole.addManagedPolicy(
+      IAM.ManagedPolicy.fromAwsManagedPolicyName('AWSStepFunctionsFullAccess')
+    );
+    lambdaRole.addManagedPolicy(
+      IAM.ManagedPolicy.fromAwsManagedPolicyName('AWSLambda_FullAccess')
+    );
+
+    const instagramPublishLambda = new NodejsFunction(
+      this,
+      'InstagramPublishHandler',
+      {
+        runtime: Lambda.Runtime.NODEJS_16_X,
+        entry: path.join(__dirname, `/../lambda/instagram.ts`),
+        handler: 'handler',
+        environment: {
+          HELLO_TABLE_NAME: '',
+        },
+        timeout: cdk.Duration.seconds(30),
+      }
+    );
+
+    const InstagramShare = new Tasks.LambdaInvoke(this, 'InstagramShare', {
+      lambdaFunction: instagramPublishLambda,
+    });
+
+    // const definition = DiscordAuthorization.next(
+    //   new StepFunctions.Choice(this, 'Is Request Authorizated?').when(
+    //     StepFunctions.Condition.numberEquals('$.Payload.StatusCode', 200),
+    //     InstagramShare
+    //   )
+    // );
+
+    const fail = new StepFunctions.Fail(this, 'Job Failed', {
+      cause: 'Share process failed',
+      error: 'Platform not okay',
+    });
+
+    const definition = new StepFunctions.Choice(this, 'Which Platform')
+      .when(
+        StepFunctions.Condition.stringEquals('$.platform', 'instagram'),
+        InstagramShare
+      )
+      .otherwise(fail);
+
+    const stateMachine = new StepFunctions.StateMachine(this, 'StateMachine', {
+      stateMachineName: 'SocialMedia',
+      definition,
+    });
+
+    const discordAuthLambda = new NodejsFunction(this, 'DiscordAuthHandler', {
+      runtime: Lambda.Runtime.NODEJS_16_X,
+      entry: path.join(__dirname, `/../lambda/discord.ts`),
+      handler: 'handler',
+      environment: {
+        STATE_MACHINE_ARN: stateMachine.stateMachineArn,
+      },
+    });
+
+    const DiscordAuthorization = new Tasks.LambdaInvoke(
+      this,
+      'DiscordAuthorization',
+      {
+        lambdaFunction: discordAuthLambda,
+      }
+    );
+
+    const discordResourse = discordApiGateway.root.addResource('discord');
+
+    const discordAuthAuthGatewayIntegration = new ApiGateway.LambdaIntegration(
+      discordAuthLambda
+    );
+
+    discordResourse.addMethod('POST', discordAuthAuthGatewayIntegration, {
+      apiKeyRequired: false,
+    });
+
+    new ApiGateway.UsagePlan(this, 'UsagePlan', {
+      name: 'TECH | Router Usage Plan',
+      apiStages: [
+        {api: discordApiGateway, stage: discordApiGateway.deploymentStage},
+      ],
+    });
   }
 }
