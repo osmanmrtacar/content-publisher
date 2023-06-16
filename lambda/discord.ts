b0f81730b2b5efc823f2f2ebef690f3ba068303b
Osman Mert ACAR <osman.acar@getir.com>
2023-06-16 22:29:40 +0300

    comment out to test

--- lambda/discord.ts
+++ lambda/discord.ts
@@ -3,42 +3,46 @@ import * as nacl from "tweetnacl";
 import * as AWS from "aws-sdk";
 
 export const handler: Handler = async (event, context) => {
-  console.log(event);
-  const PUBLIC_KEY = process.env.PUBLIC_KEY;
-  const signature = event.headers["x-signature-ed25519"];
-  const timestamp = event.headers["x-signature-timestamp"];
-  const strBody = event.body; // should be string, for successful sign
+  // console.log(event);
+  // const PUBLIC_KEY = process.env.PUBLIC_KEY;
+  // const signature = event.headers["x-signature-ed25519"];
+  // const timestamp = event.headers["x-signature-timestamp"];
+  // const strBody = event.body; // should be string, for successful sign
 
-  if (!PUBLIC_KEY) {
-    return {
-      statusCode: 401,
-      body: JSON.stringify("invalid request signature"),
-    };
-  }
-  const isVerified = nacl.sign.detached.verify(
-    Buffer.from(timestamp + strBody),
-    Buffer.from(signature, "hex"),
-    Buffer.from(PUBLIC_KEY, "hex")
-  );
-  console.log(JSON.stringify({ isVerified }));
-  if (!isVerified) {
-    return {
-      statusCode: 401,
-      body: JSON.stringify("invalid request signature"),
-    };
-  }
+  // if (!PUBLIC_KEY) {
+  //   return {
+  //     statusCode: 401,
+  //     body: JSON.stringify("invalid request signature"),
+  //   };
+  // }
+  // const isVerified = nacl.sign.detached.verify(
+  //   Buffer.from(timestamp + strBody),
+  //   Buffer.from(signature, "hex"),
+  //   Buffer.from(PUBLIC_KEY, "hex")
+  // );
+  // console.log(JSON.stringify({ isVerified }));
+  // if (!isVerified) {
+  //   return {
+  //     statusCode: 401,
+  //     body: JSON.stringify("invalid request signature"),
+  //   };
+  // }
 
-  // Replying to ping (requirement 2.)
-  const body = JSON.parse(strBody);
-  console.log({ body });
-  if (body.type === 1) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ type: 1 }),
-    };
-  }
+  // // Replying to ping (requirement 2.)
+  // const body = JSON.parse(strBody);
+  // console.log({ body });
+  // if (body.type === 1) {
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify({ type: 1 }),
+  //   };
+  // }
 
-  console.log(JSON.stringify({ isShare: body.data.name === "share" }));
+  // console.log(JSON.stringify({ isShare: body.data.name === "share" }));
+
+  const strBody = event.body;
+
+  const body = JSON.parse(strBody)
 
   if (body.data.name === "share") {
     try {
